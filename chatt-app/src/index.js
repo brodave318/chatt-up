990d3e38c643582f2dff7935937bae5b8050a056
brodave318 <brodave318@gmail.com>
2020-06-09 15:12:59 -0700

    Add HTML manipulation to rooms

--- chatt-app/src/index.js
+++ chatt-app/src/index.js
@@ -36,28 +36,33 @@ io.on('connection', (socket) => {
     socket.join(user.room)
 
     // #156 a
-    socket.emit('message', generateMessage('Welcome!'))
+    socket.emit('message', generateMessage('Admin', 'Welcome!'))
     // #157 a.a
-    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+    socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
 
     callback()
   })
 
   // #156 b.c #159
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
     // #159
     const filter = new Filter()
+
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!!')
     }
+
     // send to all connected clients
-    io.emit('message', generateMessage(message))
+    io.to(user.room).emit('message', generateMessage(user.username, message))
     callback(/*'Delivered'*/)
   })
 
   // #158 d
   socket.on('sendLocation', (coords, callback) => {
-    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?=${coords.latitude},${coords.longitude}`))
+    const user = getUser(socket.id)
+
+    io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?=${coords.latitude},${coords.longitude}`))
     callback()
   })
 
@@ -66,7 +71,7 @@ io.on('connection', (socket) => {
     const user = removeUser(socket.id)
 
     if (user) {
-      io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+      io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
     }
   })
 })
