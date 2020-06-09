c0b7ca66d1ebe2966fcab8d8d01a925db6039b3b
brodave318 <brodave318@gmail.com>
2020-06-09 16:44:49 -0700

    Clean app for deployment

--- chatt-app/src/index.js
+++ chatt-app/src/index.js
@@ -1,28 +1,20 @@
 const path = require('path')
-// #154 a
 const http = require('http')
 const express = require('express')
-// #154 b
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
-// #154 a
 const server = http.createServer(app)
-// #154 b
 const io = socketio(server)
-// #159
-const Filter = require('bad-words')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const port = process.env.PORT || 3000
-// #152 a
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-// #152 a
 app.use(express.static(publicDirectoryPath))
 
-// #154 c
 io.on('connection', (socket) => {
   console.log('Socket.io connected')
 
@@ -35,9 +27,7 @@ io.on('connection', (socket) => {
 
     socket.join(user.room)
 
-    // #156 a
     socket.emit('message', generateMessage('Admin', 'Welcome!'))
-    // #157 a.a
     socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
     io.to(user.room).emit('roomData', {
       room: user.room,
@@ -47,10 +37,8 @@ io.on('connection', (socket) => {
     callback()
   })
 
-  // #156 b.c #159
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id)
-    // #159
     const filter = new Filter()
 
     if (filter.isProfane(message)) {
@@ -62,7 +50,6 @@ io.on('connection', (socket) => {
     callback(/*'Delivered'*/)
   })
 
-  // #158 d
   socket.on('sendLocation', (coords, callback) => {
     const user = getUser(socket.id)
 
@@ -70,7 +57,6 @@ io.on('connection', (socket) => {
     callback()
   })
 
-  // #157 a.b
   socket.on('disconnect', () => {
     const user = removeUser(socket.id)
 
@@ -84,7 +70,6 @@ io.on('connection', (socket) => {
   })
 })
 
-// #154 a
 server.listen(port, () => {
   console.log(`Server is RRRUUUNNNIIINNNGG on port ${port}`)
 })
